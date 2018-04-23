4e89f091c2db62c8d3c445515a50f5d7cc8c6b35
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PreviewClickSample/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using DevExpress.Xpf.Core;
+
+namespace PreviewClickSample {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+        protected override void OnActivated(EventArgs e) {
+            ThemeManager.ApplicationThemeName = "Azure";
+        }
+    }
+}
