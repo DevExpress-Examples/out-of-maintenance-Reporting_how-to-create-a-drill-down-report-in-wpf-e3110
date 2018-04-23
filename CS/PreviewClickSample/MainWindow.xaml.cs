b99adea919d3cf7d0db5e3cfce769493abe24323
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PreviewClickSample/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Printing;
+using DevExpress.XtraReports.UI;
+// ...
+
+namespace PreviewClickSample {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        readonly DrillDownReport report = new DrillDownReport();
+
+        XtraReport Report {
+            get {
+                return report;
+            }
+        }
+
+        public MainWindow() {
+            InitializeComponent();
+            ThemeManager.ApplicationThemeName = "Azure";
+            Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e) {
+            XtraReportPreviewModel model = new XtraReportPreviewModel(report);
+            model.PreviewMouseMove += model_PreviewMouseMove;
+            model.PreviewClick += model_PreviewClick;
+            preview.Model = model;
+            report.CreateDocument(false);
+        }
+
+        void model_PreviewClick(object sender, PreviewClickEventArgs e) {
+            if (e.ElementTag != string.Empty) {
+                List<int> expandedValues =
+                    (List<int>)report.Parameters["expandedValuesParameter"].Value;
+                int index;
+                if (!int.TryParse(e.ElementTag, out index))
+                    return;
+
+                if (expandedValues.Contains(index)) {
+                    expandedValues.Remove(index);
+                }
+                else {
+                    expandedValues.Add(index);
+                }
+                Report.CreateDocument(false);
+            }
+        }
+
+        void model_PreviewMouseMove(object sender, PreviewClickEventArgs e) {
+            if (e.ElementTag != null && !e.ElementTag.Equals(string.Empty)) {
+                e.Control.Cursor = Cursors.Hand;
+            }
+        }
+    }
+}
---
+++ VB/PreviewClickSample/MainWindow.xaml.vb
@@ -0,0 +1,63 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Input
+Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Printing
+Imports DevExpress.XtraReports.UI
+' ...
+
+Namespace PreviewClickSample
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+
+		Private ReadOnly report_Renamed As New DrillDownReport()
+
+		Private ReadOnly Property Report() As XtraReport
+			Get
+				Return report_Renamed
+			End Get
+		End Property
+
+		Public Sub New()
+			InitializeComponent()
+			ThemeManager.ApplicationThemeName = "Azure"
+			AddHandler Loaded, AddressOf MainWindow_Loaded
+		End Sub
+
+		Private Sub MainWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim model As New XtraReportPreviewModel(report_Renamed)
+			AddHandler model.PreviewMouseMove, AddressOf model_PreviewMouseMove
+			AddHandler model.PreviewClick, AddressOf model_PreviewClick
+			preview.Model = model
+			report_Renamed.CreateDocument(False)
+		End Sub
+
+		Private Sub model_PreviewClick(ByVal sender As Object, ByVal e As PreviewClickEventArgs)
+			If e.ElementTag <> String.Empty Then
+				Dim expandedValues As List(Of Integer) = CType(report_Renamed.Parameters("expandedValuesParameter").Value, List(Of Integer))
+				Dim index As Integer
+				If (Not Integer.TryParse(e.ElementTag, index)) Then
+					Return
+				End If
+
+				If expandedValues.Contains(index) Then
+					expandedValues.Remove(index)
+				Else
+					expandedValues.Add(index)
+				End If
+				Report.CreateDocument(False)
+			End If
+		End Sub
+
+		Private Sub model_PreviewMouseMove(ByVal sender As Object, ByVal e As PreviewClickEventArgs)
+			If e.ElementTag IsNot Nothing AndAlso (Not e.ElementTag.Equals(String.Empty)) Then
+				e.Control.Cursor = Cursors.Hand
+			End If
+		End Sub
+	End Class
+End Namespace
